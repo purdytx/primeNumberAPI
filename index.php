cc276db7614191c1166f461c6980ac00dcf5a01a
root <root@folkvangr.(none)>
2016-08-04 04:51:20 +0000

    adding coors for ease of use, if you need secure swap out for oauth

--- index.php
+++ index.php
@@ -24,6 +24,32 @@ $di = new FactoryDefault();
 
 $app = new Micro($di);
 
+/**
+*  for quick and dirty api, enabling COORS
+*  for a secure api swap this out with a proper secure call such as oauth token exhange.
+*/
+$response = $app->response;                      
+$response->setHeader('Access-Control-Allow-Origin', '*');
+$response->setHeader('Access-Control-Allow-Headers', 'X-Requested-With');      
+$response->sendHeaders();
+
+$app->get('/preflight', function() use ($app) {
+        $content_type = 'application/json';
+        $status = 200;
+        $description = 'OK';
+        $response = $app->response;
+
+        $status_header = 'HTTP/1.1 ' . $status . ' ' . $description;
+        $response->setRawHeader($status_header);
+        $response->setStatusCode($status, $description);
+        $response->setContentType($content_type, 'UTF-8');
+        $response->setHeader('Access-Control-Allow-Origin', '*');
+        $response->setHeader('Access-Control-Allow-Headers', 'X-Requested-With');
+        $response->setHeader("Access-Control-Allow-Headers: Authorization");
+        $response->setHeader('Content-type: ' . $content_type);
+        $response->sendHeaders();
+    });
+
 //routing here
 $app->get('/api/prime/{start:[0-9]+}/{end:[0-9]+}', function($start, $end) use ($app) {
 
